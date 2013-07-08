65b87e90d042f697a8eae2564da9d92edc83c893
andris310 <andris310@gmail.com>
2013-07-07 21:51:23 -0700

    Initial commit

---
+++ lacozi_wh/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LacoziWh::Application.config.secret_token = '7231a4201847b498a00608f016bf71b3710c75eb79e5ec56bf03150af8a56af6e312737677ed352303402db729e03007124396ae421418fefd1cebeeb4d89815'
