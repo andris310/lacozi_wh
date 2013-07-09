a50a0eacbcca17079e38e3aa7fb953d6c767b635
andris310 <andris310@gmail.com>
2013-07-09 10:07:21 -0700

    dump shcema

--- lacozi_wh/db/schema.rb
+++ lacozi_wh/db/schema.rb
@@ -13,13 +13,4 @@
 
 ActiveRecord::Schema.define(:version => 20130708201255) do
 
-  create_table "products", :id => false, :force => true do |t|
-    t.string   "name"
-    t.text     "description"
-    t.string   "code"
-    t.string   "category"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
-  end
-
 end
