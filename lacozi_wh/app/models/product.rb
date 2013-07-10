ac25517c38a66951cf328c7a72f3bb7b9557929e
andris310 <andris310@gmail.com>
2013-07-10 15:06:45 -0700

    Added category_id

--- lacozi_wh/app/models/product.rb
+++ lacozi_wh/app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
   mount_uploader :picture, PictureUploader
-  attr_accessible :category, :code, :description, :name, :picture
+  attr_accessible :category_id, :category, :code, :description, :name, :picture
   belongs_to :category
 end
