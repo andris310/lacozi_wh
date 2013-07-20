8d0f51474eaccaffed6e99ef83bf33483ec7880e
andris310 <andris310@gmail.com>
2013-07-20 10:34:05 -0700

    added slider javasript

---
+++ lacozi_wh/app/assets/javascripts/swipe_control.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  window.mySwipe = Swipe(document.getElementById('slider'), {
+    startSlide: 2,
+    speed: 600,
+    auto: 3000,
+    continuous: true,
+    disableScroll: false,
+    stopPropagation: false,
+    callback: function(index, elem) {},
+    transitionEnd: function(index, elem) {}
+  });
+});
